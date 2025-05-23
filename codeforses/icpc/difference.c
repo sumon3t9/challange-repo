00e47384976ffd79423118790d88e9196224f7ef
Md Sumon Rana <mdshumonrana8@gmail.com>
2025-05-23 19:20:45 +0600

    Delete codeforses/icpc directory

--- codeforses/icpc/difference.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    long long int A, B, C, D, X;
-    scanf("%lld %lld %lld %lld", &A, &B, &C, &D);
-    X =(A * B) - (C * D);
-    printf("Difference = %lld\n",X);
-    return 0;
-}
--- codeforses/icpc/hello.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    char S[101];
-    scanf("%100s", S);
-    printf("Hello, %s\n", S);
-    return 0;
-}
