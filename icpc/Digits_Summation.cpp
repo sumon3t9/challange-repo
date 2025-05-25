1fffa2d60ef83abf045f4ea2d06b77c8d23f79b6
Md Sumon Rana <mdshumonrana8@gmail.com>
2025-05-25 12:59:35 +0600

    Add files via upload

---
+++ icpc/Digits_Summation.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    long long int X, Y, M, N, Sum;
+    cin >> X >> Y;
+    M = (X % 10);
+    N = (Y % 10);
+    Sum = (M + N);
+ cout << Sum << '\n';
+    return 0;
+}
