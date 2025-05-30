461df0105f545e29593645956dc6d67bf67c9481
Md Sumon Rana <mdshumonrana8@gmail.com>
2025-05-30 22:39:00 +0600

    Add files via upload

---
+++ sheet2/Multification.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    for (int i = 1; i <= 12; ++i) {
+        cout << N << " * " << i << " = " << N * i << endl;
+    }
+    return 0;
+}
