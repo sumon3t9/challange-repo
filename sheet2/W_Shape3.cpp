6e613622f0db14e2ea1e68f5ebabdbd9af7c7c07
Md Sumon Rana <mdshumonrana8@gmail.com>
2025-06-16 22:59:57 +0600

    W_Shape3

---
+++ sheet2/W_Shape3.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    for (int i = 1; i <= N; ++i) {
+        for (int j = 1; j <= N - i; ++j) cout << ' ';
+        for (int k = 1; k <= 2 * i - 1; ++k) cout << '*';
+        cout << '\n';
+    }
+    for (int i = N ; i >= 1; --i) 
+    {
+        for (int j = 1; j <= N - i; ++j) cout << ' ';
+        for (int k = 1; k <= 2 * i - 1; ++k) cout << '*';
+        cout << '\n';
+    }
+    return 0;
+}
