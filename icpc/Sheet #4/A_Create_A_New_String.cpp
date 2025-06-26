ee233106fb0f3854d3cfd18e22ee1d0ad8e7c990
Md Sumon Rana <mdshumonrana8@gmail.com>
2025-06-26 22:06:19 +0600

    A_Create_A_New_String

---
+++ icpc/Sheet #4/A_Create_A_New_String.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string S, T;
+    getline(cin, S);
+    getline(cin, T);
+
+    cout << S.length() << " " << T.length() << endl;
+    cout << S << " " << T << endl;
+
+    return 0;
+}
