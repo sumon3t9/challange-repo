017f4011c3349e9bf8c960c5f6e1389f8e378671
Md Sumon Rana <mdshumonrana8@gmail.com>
2025-06-27 23:07:51 +0600

    C_Compare

---
+++ icpc/Sheet #4/C_Compare.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string X, Y;
+    cin >> X >> Y;
+    if (X < Y)
+        cout << X << endl;
+    else
+        cout << Y << endl;
+    return 0;
+}
