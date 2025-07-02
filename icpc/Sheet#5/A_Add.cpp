c5a6b14d999f40716362018a23e1d71381dff9ad
Md Sumon Rana <mdshumonrana8@gmail.com>
2025-07-02 23:20:07 +0600

    A_Add

---
+++ icpc/Sheet#5/A_Add.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int add(int x, int y) {
+    return x + y;
+}
+
+int main() {
+    int X, Y;
+    cin >> X >> Y;
+    cout << add(X, Y) << endl;
+    return 0;
+}
