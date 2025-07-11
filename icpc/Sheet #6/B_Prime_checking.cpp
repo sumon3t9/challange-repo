f4846046214fe9f73f36450fda36a0474fbe8ba7
Md Sumon Rana <mdshumonrana8@gmail.com>
2025-07-11 22:49:56 +0600

    B_Prime_checking

---
+++ icpc/Sheet #6/B_Prime_checking.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPrime(long long n) {
+    if (n < 2) return false;
+    if (n == 2 || n == 3) return true;
+    if (n % 2 == 0 || n % 3 == 0) return false;
+    for (long long i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    }
+    return true;
+}
+
+int main() {
+    long long N;
+    cin >> N;
+    if (isPrime(N))
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+    return 0;
+}
