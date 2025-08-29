ffa4791520de5b381c09607f55b398627008a8ae
Md Sumon Rana <mdshumonrana8@gmail.com>
2025-08-29 18:32:01 +0600

    A_Lucky

---
+++ codefors-problem-solve/A_Lucky.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string ticket;
+        cin >> ticket;
+        int sum1 = ticket[0] - '0' + ticket[1] - '0' + ticket[2] - '0';
+        int sum2 = ticket[3] - '0' + ticket[4] - '0' + ticket[5] - '0';
+        if (sum1 == sum2)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+    return 0;
+}
