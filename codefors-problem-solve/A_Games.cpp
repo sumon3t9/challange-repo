6875d5dc02c1e160e658e4416bf3424485a73d21
Md Sumon Rana <mdshumonrana8@gmail.com>
2025-09-08 19:38:04 +0600

    A_Games

---
+++ codefors-problem-solve/A_Games.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<pair<int, int>> teams(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> teams[i].first >> teams[i].second; 
+    }
+
+    int count = 0;
+    
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (i == j) continue;
+            
+            if (teams[i].first == teams[j].second) {
+                count++;
+            }
+        }
+    }
+    cout << count << endl;
+    return 0;
+}
